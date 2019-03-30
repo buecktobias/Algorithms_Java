48319f39d3dbe23be359b8f817f978b4b437c2d7
buecktobias <44665917+buecktobias@users.noreply.github.com>
2019-03-30 10:56:43 +0100

    deleted stupid code

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,9 +1,4 @@
 public class QuickSort {
-    public static void swapTwoElements(int[] intArray,int index1,int index2){
-        int temp = intArray[index1];
-        intArray[index1] = intArray[index2];
-        intArray[index2] = temp;
-    }
     public static void quickSort(int[] intArray){
         quickSort(intArray,0,intArray.length - 1);
     }
@@ -23,12 +18,12 @@ public class QuickSort {
                 rightIndex--;
             }
             if(leftIndex < rightIndex){
-                swapTwoElements(intArray,leftIndex,rightIndex);
+                Helper.swapTwoElements(intArray,leftIndex,rightIndex);
             }
 
         }while(leftIndex < rightIndex);
         if(intArray[leftIndex] > pivotValue){
-            swapTwoElements(intArray,leftIndex,startedRightIndex);
+            Helper.swapTwoElements(intArray,leftIndex,startedRightIndex);
         }
         quickSort(intArray,startedLeftIndex,leftIndex-1);
         quickSort(intArray,leftIndex + 1,startedRightIndex);
